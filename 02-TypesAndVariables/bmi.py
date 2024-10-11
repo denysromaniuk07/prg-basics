f39fe87bda847b49667d2145fc226961b85b4452
Denys Romaniuk <237703@student.uek.krakow.pl>
2024-10-11 12:21:50 +0200

    11 10 24(2ND S)

---
+++ 02-TypesAndVariables/bmi.py
@@ -0,0 +1,24 @@
+# import random
+# print("Dice rolling simulator")
+# for i in range(5):
+#     dice_roll = random.randint(1,6)
+#     print(dice_roll, end=" ")
+
+# a = 1 + 2 % 3 ** 4 * 5
+# print(a)
+
+
+###
+# A program that calculates the sum of two numbers.
+# Modify the program to calculate the sum of three numbers.
+
+# number1 = 71
+# number2 = 14
+# number3 = 10
+# result = number1 + number2 + number3 
+# print('Number 1: ', number1)
+# print('Number 2: ', number2)
+# print('Number 3: ', number3)
+# print('The result of summation: ', result)
+
+
