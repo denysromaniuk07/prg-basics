82877147f806991b5c51274091691fe6993b2a25
Denys Romaniuk <237703@student.uek.krakow.pl>
2024-10-18 11:21:40 +0200

    18 10 first s

---
+++ 03-ControlStructures/1.py
@@ -0,0 +1,67 @@
+### 1.3
+# Checking whether a car exceeded the speed limit
+
+# speed_limit = 140
+# car_speed = int( input('Enter car speed (km/h): ') )
+
+# if car_speed >speed_limit:
+#     print(f'Your speed is {car_speed}km/h')
+#     print('Warning: speed limit exceeded!!')
+
+### 1.4 
+# Credit card payment 
+#
+# account_balance = 500
+# total_payment = int(input("Enter total paymens: "))
+
+# if total_payment <= account_balance:
+#     print('Payment completed')
+# else:
+#     print('No funds')
+
+### 1.5
+# Checking whether the test is passed
+# Test is passed when the number of correctly completed
+# tasks is at least 50%
+#
+# total_tasks = 20
+# tasks_ok = int(input("Enter correct answers: "))
+# test_passed = False
+
+# if tasks_ok >= total_tasks/2 :
+#     test_passed = True
+
+# if test_passed:
+#     print('Congratulations! You passed the test.')
+# else:
+#     print('Unfortunately, you failed the test.')
+
+### 1.6
+# Checking whether the number
+# entered from the keyboard is even or odd 
+#
+# number = int(input('Enter number: '))
+
+# if number % 2 == 0:
+#     print(f'{number} is even')
+# else:
+#     print(f'{number} is odd')
+
+### 1.7
+# Program that calculates the employee's salary,
+# taking into account the possibility of receiving a bonus.
+#
+# basic_salary = 5000
+# total_salary = 0
+# is_bonus = True # does the employee receive a bonus
+# bonus = 0.30 # 15%
+
+# if is_bonus:
+#     total_salary = basic_salary + bonus*basic_salary
+# else:
+#     total_salary = basic_salary
+
+# print(f'Basic salary: {basic_salary}')
+# print(f'Bonus included? {is_bonus}')
+# print(f'Total salary: {total_salary}')
+
