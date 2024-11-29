314c2eda308a4cbec49a2f7d1e54a54e8a706cee
s-A013-34 <s-A013-34@CAMPUS.UEK.KRAKOW.PL>
2024-11-29 12:31:27 +0100

    292111

--- 08-FileHandling/2.py
+++ 08-FileHandling/2.py
@@ -13,3 +13,21 @@ with open(file_name, 'w') as file:
    file.write(f"Lead Actor: {lead_actor}\n")
 
 print(f"Movie details have been written to {file_name}.")
+
+###2.3
+# Makes a copy of a text file
+#
+
+# file names
+original_file = 'healthy_lifestyle.txt'
+target_file = 'copy_healthy_lifestyle.txt'
+
+# read the content of the original file
+with open(original_file) as file:
+   content = ... .read()
+...
+...
+
+# write the content to the target file (copy)
+with ... as ...:
+   ... .write(...)
